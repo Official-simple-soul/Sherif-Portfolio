69bf3ed93c6173693c24c94f7105c00d454a19b8
Official-simple-soul <abdurroheemsherifdeen@gmail.com>
2022-12-24 08:32:50 +0100

    working on a new portfolio

---
+++ components/Hero.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import dynamic from "next/dynamic";
+const Animator = dynamic(
+  import("react-scroll-motion").then((it) => it.Animator),
+  { ssr: false }
+);
+
+import { ScrollContainer, ScrollPage, batch, Fade, FadeIn, FadeOut, Move, MoveIn, MoveOut, Sticky, SStickyIn, StickyOut, Zoom, ZoomIn, ZoomOut } from "react-scroll-motion";
+
+
+export default function Hero() {
+  return (
+    <ScrollContainer>
+        <ScrollPage>
+            <div className='h-screen flex justify-center items-center'>
+                <div className="text-center">
+                    <h1 className='text-3xl font-bold'><Animator animation={MoveIn(-1000, 0)}>Hello Guys 👋🏻</Animator></h1>
+                    <h1 className='text-3xl font-bold text-[#4b0303]'><Animator animation={batch(Sticky(), Fade())}>Nice to meet you 🙋🏻‍♀️</Animator></h1>
+                    <p className='mt-12 underline'><Animator animation={MoveOut(1000, 0)}>Scroll down to continue</Animator></p>
+                    <p className='mt-12 underline'><Animator animation={MoveOut(1000, 0)}>Enjoy</Animator></p>
+                </div>
+            </div>
+        </ScrollPage>
+    </ScrollContainer>
+  )
+}
