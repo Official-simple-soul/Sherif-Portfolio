4e1c26683f614132e671facfd750a2601867f059
Official-simple-soul <abdurroheemsherifdeen@gmail.com>
2022-12-25 02:21:48 +0100

    still in progress

--- components/Simple.jsx
+++ components/Simple.jsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 import SimpleImage from '../public/assets/images/SimpleVectorImage.jpg'
 export default function Simple() {
     return (
-            <div className="relative text-center w-full flex justify-center items-center h-screen">
+            <div className="relative text-center w-full flex justify-center items-center h-screen" id="about">
               <div className="col">
               <span style={{ fontSize: "40px" }} className='z-50'>I'm Simple_Soul ✨</span>
               <Image
